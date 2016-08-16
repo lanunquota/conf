2fc135f51c01c0288d6042bbd957c7e7f7549a1d
lanunquota <izamlukman.93@gmail.com>
2016-08-16 10:45:32 -0700

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -25,8 +25,6 @@ wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/lanunquota/conf
 MYIP=`curl -s ifconfig.me`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 sed -i $MYIP2 /etc/squid/squid.conf;
-sed -i '$ a nameserver 8.8.8.8' /etc/resolv.conf;
-sed -i '$ a nameserver 8.8.4.4' /etc/resolv.conf;
 chkconfig squid on
 service squid restart
 
